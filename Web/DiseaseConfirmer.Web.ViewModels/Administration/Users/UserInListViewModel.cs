3889877adb6945eb0be95ec428cc4988315caf92
Kalin Kostadinov <58563576+kalin10@users.noreply.github.com>
2020-12-01 22:08:52 +0200

    Changes in Users/All.cshtml

--- Web/DiseaseConfirmer.Web.ViewModels/Administration/Users/UserInListViewModel.cs
+++ Web/DiseaseConfirmer.Web.ViewModels/Administration/Users/UserInListViewModel.cs
@@ -8,5 +8,11 @@
         public string Id { get; set; }
 
         public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
     }
 }
